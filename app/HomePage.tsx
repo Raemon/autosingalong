96677e280a2645f203a4e682d6aa58b4b12c6093
Raemon <raemon777@gmail.com>
2025-12-31 19:56:26 -0800

    test

--- app/HomePage.tsx
+++ app/HomePage.tsx
@@ -23,11 +23,11 @@ const HomePage = () => {
       </div>
       <div className="lg:w-1/2 max-w-xl flex flex-col gap-12 pb-12 pt-20">
         <div>
-          <h3 className="font-georgia text-2xl mb-2 border-b border-gray-500 pb-2">Songs/Speeches</h3>
+          <h3 className="font-georgia text-3xl mb-2 border-b border-gray-500 pb-2">Songs/Speeches</h3>
           <RecentSongs />
         </div>
         <div>
-          <h3 className="font-georgia text-2xl mb-2 border-b border-gray-500 pb-2">Programs</h3>
+          <h3 className="font-georgia text-3xl mb-2 border-b border-gray-500 pb-2">Programs</h3>
           <RecentPrograms />
         </div>
       </div>
